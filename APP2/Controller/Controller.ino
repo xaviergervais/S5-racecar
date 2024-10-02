81ccdfe505aa10901b4cc836348f800f2fc32f4e
Samgd14 <samgd14@live.ca>
2024-10-01 23:53:44 -0400

    Added the PIDs!

--- APP2/Controller/Controller.ino
+++ APP2/Controller/Controller.ino
@@ -105,6 +105,7 @@ signed long enc_now   = 0;
 signed long enc_old   = 0;
 
 float pos_now   = 0;
+float pos_old   = 0;
 float vel_now   = 0;
 float vel_old   = 0;
 
@@ -369,7 +370,7 @@ void ctl(int dt_low){
     //TODO: VOUS DEVEZ COMPLETEZ LE CONTROLLEUR SUIVANT
     vel_ref       = dri_ref; 
     vel_error     = vel_ref - vel_fil;
-    vel_error_int = 0; // TODO
+    vel_error_int = vel_error_int + (vel_ref - vel_fil) * dt_low / 1000;
     dri_cmd       = vel_kp * vel_error; // proportionnal only
     
     dri_pwm    = cmd2pwm( dri_cmd ) ;
@@ -384,16 +385,16 @@ void ctl(int dt_low){
 
     //TODO: VOUS DEVEZ COMPLETEZ LE CONTROLLEUR SUIVANT
     pos_ref       = dri_ref; 
-    pos_error     = 0; // TODO
-    pos_error_ddt = 0; // TODO
-    pos_error_int = 0; // TODO
+    pos_error     = dri_ref - pos_now;
+    pos_error_ddt = (pos_error - (dri_ref - pos_old)) / dt_low * 1000;
+    pos_error_int = pos_error_int + (dri_ref - pos_now) * dt_low / 1000; 
     
     // Anti wind-up
     if ( pos_error_int > pos_ei_sat ){
       pos_error_int = pos_ei_sat;
     }
     
-    dri_cmd = 0; // TODO
+    dri_cmd = pos_error*pos_kp + pos_error_int*pos_ki + pos_error_ddt*pos_kd;
     
     dri_pwm = cmd2pwm( dri_cmd ) ;
   }
@@ -424,6 +425,7 @@ void ctl(int dt_low){
   
   //Update memory variable
   enc_old = enc_now;
+  pos_old = pos_now;
   vel_old = vel_fil;
 }
 
